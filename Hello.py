3a95ecb0337aed209d2ec502d0b979238e508495
Dhawal Chheda <DChheda@infinera.com>
2018-01-27 23:28:37 -0800

    init commit

--- Hello.py
+++ Hello.py
@@ -4,3 +4,8 @@ if name == "Dhawal":
     print("Nice to meet you {0}. I am {1}".format(name, machine))
 else:
     print("Nope")
+
+letters = ["A", "B", "C"]
+print(letters[1:])
+for letter in letters:
+    print("Letter is {0}".format(letter))
